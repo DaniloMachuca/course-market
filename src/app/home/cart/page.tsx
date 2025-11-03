c4353adad9b81d4fd5953c8d2932a87f0ca8fa8c
Danilo Machuca <danilo.machuca.dev@gmail.com>
2025-11-03 13:33:09 -0300

    add: mensagem de carrinho vazio e resumo da compra

--- src/app/home/cart/page.tsx
+++ src/app/home/cart/page.tsx
@@ -3,9 +3,12 @@
 import CartItem from "@/components/CartItem";
 import { useSelector } from "react-redux";
 import { RootReducer } from "@/store";
+import Link from "next/link";
+import { parseToBRL } from "@/utils";
 
 const CartPage = () => {
   const { list } = useSelector((state: RootReducer) => state.cart);
+  const total = list.reduce((acc, course) => acc + course.price, 0);
 
   return (
     <div className="container mx-auto p-8 max-w-6xl min-h-screen grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -13,17 +16,39 @@ const CartPage = () => {
         <h3 className="text-3xl font-bold mb-8 text-gray-800">
           Carrinho de compras
         </h3>
-        <div className="flex flex-col">
-          {list.map((course) => (
-            <CartItem key={course.id} course={course} />
-          ))}
-        </div>
+        {list.length === 0 ? (
+          <>
+            <p className="text-2xl mb-8">
+              Você não possui cursos no carrinho no momento
+            </p>
+            <Link
+              href="/home/courses"
+              className="text-2x1 bg-primary text-white py-2 px-3 rounded hover:bg-primary-dark transition-colors hover:cursor-pointer"
+            >
+              Procurar por Cursos
+            </Link>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col">
+              {list.map((course) => (
+                <CartItem key={course.id} course={course} />
+              ))}
+            </div>
+          </>
+        )}
       </main>
-      <aside>
-        <h3 className="text-3xl font-bold col-span-1 mb-8 text-gray-800">
-          Resumo da compra
-        </h3>
-      </aside>
+      {list.length === 0 ? null : (
+        <aside>
+          <h3 className="text-3xl font-bold col-span-1 mb-8 text-gray-800">
+            Resumo da compra
+          </h3>
+          <p className="text-2xl mb-8">Valor total: {parseToBRL(total)}</p>
+          <button className="bg-primary text-white py-2 px-4 rounded hover:bg-primary-dark cursor-pointer hover:cursor-pointer transition-colors duration-300">
+            Finalizar compra
+          </button>
+        </aside>
+      )}
     </div>
   );
 };
